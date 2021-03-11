095c5df5dcd3081530ec754647494832ba79a91b
Benito <bbxblack@gmail.com>
2021-03-11 11:48:08 +0100

    esercizio live

--- js/script.js
+++ js/script.js
@@ -6,14 +6,49 @@ priceBtn.addEventListener('click', function() {
   if (burgerName.length < 1) {
     alert('prego inserire nome panino');
   } else {
-    var checkboxs = document.getElementsByClassName('ingred');
+    var checkboxs = document.getElementsByClassName('ingred'); //abbiamo preso tutti i valori delle checkbox
 
-    for (var i = 0; i < checkboxs.length; i++) {
+    var finalPrice = 10;
+
+    for (var i = 0; i < checkboxs.length; i++) { // apriamo un ciclo for per poter estrarre il valore contenuto all'interno di ogni singola checkbox
 
       var checkbox = checkboxs[i];
       var isChecked = checkbox.checked;
+      var price = parseInt(checkbox.dataset.price);
 
-      console.log(checkbox, isChecked);
+      if (isChecked) {
+
+        finalPrice +=price;
+
+      }
     }
+
+    var coupons = [
+      '123456ABCDEF',
+      '59478HUIDDIK',
+      '86921UTVDNIO'
+    ]
+
+    var burgerCoupon = getElementById('burger-coupon').value;
+    if (coupons.inculed(burgerCoupon)) {
+      finalPrice -= finalPrice * 0.2;
+    }
+    // for (var i = 0; i < coupons.length; i++) {
+    //   var coupon = coupons[i];
+    //   if (coupon == burgerCoupon) {
+    //
+    //     finded = true;
+    //
+    //   }
+    // }
+    // if (finded) {
+    //
+    //   finalPrice -= finalPrice * 0.2; // finalPrice = finalPrice - ((finalPrice / 100) * 20);
+    //
+    // }
+
+    var spanPrice = document.getElementById('price');
+    spanPrice.innerHTML = finalPrice;
+
   }
 });
